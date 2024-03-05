334781788bcb5889a9a9b206676b597f096dccf0
Jere1 <monserratjere@gmail.com>
2024-03-05 18:29:57 -0300

    repositorio react

---
+++ src/data/Tasks.js
@@ -0,0 +1,27 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primer tarea",
+    description: "mi primer tarea",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "mi segunda tarea",
+  },
+  {
+    id: 2,
+    tittle: "Mi tercer tarea",
+    description: "mi tercer tarea",
+  },
+  {
+    id: 3,
+    title: "Mi cuarta tarea",
+    description: "mi cuarta tarea",
+  },
+  {
+    id: 4,
+    title: "Mi quinta tarea",
+    description: "mi quinta tarea",
+  },
+];
